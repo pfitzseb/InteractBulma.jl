1c2090fc02210bf0f51a6a375f6253e63eed7253
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2018-10-18 21:42:59 +0200

    customizeable css

--- deps/build.jl
+++ deps/build.jl
@@ -4,15 +4,4 @@ const _pkg_assets = joinpath(_pkg_root,"assets")
 
 !isdir(_pkg_assets) && mkdir(_pkg_assets)
 
-deps = [
-    "https://raw.githubusercontent.com/piever/BulmaThemes/master/public/styles/main.css",
-    "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css",
-    "https://cdn.jsdelivr.net/npm/bulma-extensions@1.0.14/bulma-slider/dist/bulma-slider.min.css",
-    "https://cdn.jsdelivr.net/npm/bulma-extensions@1.0.14/bulma-switch/dist/bulma-switch.min.css",
-    "https://cdn.jsdelivr.net/npm/bulma-extensions@1.0.14/bulma-checkradio/dist/bulma-checkradio.min.css",
-    "https://cdn.jsdelivr.net/npm/bulma-extensions@1.0.14/bulma-accordion/dist/bulma-accordion.min.css",
-    "https://cdn.jsdelivr.net/npm/bulma-extensions@1.0.14/bulma-tooltip/dist/bulma-tooltip.min.css",
-]
-for dep in deps
-    download(dep, joinpath(_pkg_assets, splitdir(dep)[2]))
-end
+download("https://github.com/jgthms/bulma/releases/download/0.7.2/bulma-0.7.2.zip", joinpath(_pkg_assets, "bulma.zip"))
